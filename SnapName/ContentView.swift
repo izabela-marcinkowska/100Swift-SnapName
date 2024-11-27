fc949609598f68863b3a23dcac1b3d23296c0517
Devy <izabela.marcinkowska@outlook.com>
2024-11-27 08:33:11 +0100

    first commit

---
+++ SnapName/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapName
+//
+//  Created by Izabela Marcinkowska on 2024-11-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapName/SnapNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapNameApp.swift
+//  SnapName
+//
+//  Created by Izabela Marcinkowska on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct SnapNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
